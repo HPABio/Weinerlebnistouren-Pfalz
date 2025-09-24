9e488b026ea73ece5f8b8cfe6312b62fd06304eb
Hendrik Cooper <biocentra.info@gmail.com>
2025-09-24 19:39:20 +0200

    Adjusted and Improved Kontakt Form. Upgraded to Web3Forms (250 monthly requests free). Added Vite to pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // astro.config.mjs
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "vite-plugin-compression";
+import { loadEnv } from "vite";
+
+const { WEB3FORMS_ACCESS_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
+  env: {
+    schema: {
+      WEB3FORMS_ACCESS_KEY: envField.string({ context: "server", access: "secret" })
+    },
+  },
   integrations: [react(), tailwind()],
   vite: {
     plugins: [
