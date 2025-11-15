07108cf40122e67e29823722ddda5595f5197d3d
Hendrik Cooper <biocentra.info@gmail.com>
2025-11-15 02:41:11 +0100

    disabled Googe Analytics in Dev mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,27 @@ import { loadEnv } from "vite";
 
 import sitemap from "@astrojs/sitemap";
 
-const { WEB3FORMS_ACCESS_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { WEB3FORMS_ACCESS_KEY } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
 
 export default defineConfig({
   env: {
     schema: {
-      WEB3FORMS_ACCESS_KEY: envField.string({ context: "server", access: "secret" })
+      WEB3FORMS_ACCESS_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
     },
   },
   site: "https://www.weinerlebnistouren-pfalz.de",
   integrations: [react(), tailwind(), sitemap()],
   vite: {
+    css: {
+      devSourcemap: false
+    },
     plugins: [
       compress({
         algorithm: "brotliCompress",
